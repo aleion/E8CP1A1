13f7a9a7cf818d5793216f00b5571de5bbed1afc
aleion <alejandro.ion.carvajal@gmail.com>
2018-12-17 20:17:53 -0300

    ejercicio 1 - Punto 1 y 2 ok

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,9 +1,25 @@
+arr = ['Elem1','Elem2','Elem3','Elem4','Elem5']
+puts 'Ingrese parrafo 1'
 string1 = gets.chomp
+puts 'Ingrese parrafo 2'
 string2 = gets.chomp
+#puts 'Ingrese el colore de los parrafos'
+#color_p = gets.chomp
 def metodo_uno(str1, str2)
     file = File.new('index.html', 'w')
-    file.write "<p>#{str1}</p>\n<p>#{str2}</p>"
+    file.write "<!DOCTYPE html>\n<html>\n<head>\n<meta charset='utf-8'/>\n<meta charset='utf-8'/>\n<meta http-equiv='X-UA-Compatible'content='IE=edge'>\n<title>Ejercicio 1</title>\n<meta name='viewport' content='width=device-width, initial-scale=1'>\n<style>\n</style>\n</head>\n<body>\n<p>#{str1}</p>\n<p>#{str2}</p>\n</body>\n</html>\n"
     file.close
+end
+def metodo_dos(arreglo)
+    
+    file = File.open('index.html', 'a')
+    file.write "<ol>\n"
+    elem = arreglo.each {|i| file.puts "<li>#{i}</li>"}
+    file.write "</ol>\n"
+    #file.puts "<ol>#{elem}\n</ol>\n"
+    file.close
+
 end
 
 metodo_uno(string1, string2)
+metodo_dos(arr)
